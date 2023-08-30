f2f0ec35e04d42053574ba999e7fe93671bd04d8
“Nickhil <suryanscrit@gmail.com>
2023-08-30 15:54:48 -0500

    functionality

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,5 +1,20 @@
+import axios from "axios";
 import { useState } from "react";
 
 export function Login() {
-  return <h1>Login</h1>;
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
+  return (
+    <div id="login">
+      <form>
+        <div>
+          Email: <input name="email" type="email" />
+        </div>
+        <div>
+          Password: <input name="password" type="password" />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 }
