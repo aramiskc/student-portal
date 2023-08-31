b1fdb77cc5a3804840027bf8d737b0cec23bad0b
luke <luke.nadackapadam@gmail.com>
2023-08-31 12:26:11 -0500

    Add resume html form

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 // import { Dashboard } from "./Dashboard";
 import { Login } from "./Login";
+import { Resume } from "./Resume";
 
 function App() {
   return (
     <div>
       {/* <Dashboard /> */}
       <Login />
+      <Resume />
     </div>
   );
 }
